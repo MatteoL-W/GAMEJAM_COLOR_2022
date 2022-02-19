60f1a08e0414c0fb1cf82af7d9610c8f88a8413a
Yaelle <yaellesaraleguitran@gmail.com>
2022-02-19 14:44:14 +0100

    [Player] intersectBall seems to compile fine

--- src/Player.cpp
+++ src/Player.cpp
@@ -12,5 +12,14 @@ void Player::shot(Point2D position)
 bool Player::intersectBall(const Point2D& positionClick, const Ball& ball, std::vector<DynamicObject*> players, std::vector<Point2D*> fieldLimits, Point2D& intersection)
 {
     nearestIntersection(positionClick, *this, players, fieldLimits, intersection);
-    return true;
+    Point2D tmpIntersection;
+    if (intersectCircle(ball.getPosition(), ball.getRadius(), this->getPosition(), this->getPosition().getDirection(positionClick), tmpIntersection)) {
+        float distanceBall    = this->getPosition().getDistance(tmpIntersection);
+        float nearestDistance = this->getPosition().getDistance(intersection);
+        if (distanceBall < nearestDistance) {
+            intersection = tmpIntersection;
+            return true;
+        }
+    }
+    return false;
 }
